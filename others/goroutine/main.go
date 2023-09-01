4a300e12e53b0f3b038b3e9d8213d6d156a89cad
jackluson <luxuemin2108@gmail.com>
2023-09-01 15:35:45 +0800

    feat: add goroutine learn

---
+++ others/goroutine/main.go
@@ -0,0 +1,100 @@
+/*
+ * Desc:
+ * File: /main.go
+ * Project: goroutine
+ * File Created: Thursday, 31st August 2023 6:15:20 pm
+ * Author: luxuemin2108@gmail.com
+ * -----
+ * Copyright (c) 2023 Camel Lu
+ */
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func A(i int) {
+	fmt.Println("我是A", i)
+}
+func SyncWaitGroupWay() {
+	var wg sync.WaitGroup
+	fmt.Println("我是main")
+	wg.Add(1)
+	// go A(1)
+	go func(i int) {
+		defer wg.Done()
+		A(i)
+		fmt.Println("after finish")
+
+	}(1)
+	wg.Wait()
+	fmt.Println("执行完了")
+}
+
+func SyncCondWay() {
+	var locker = new(sync.Mutex)
+	var cond = sync.NewCond(locker)
+	var done bool = false
+	fmt.Println("我是main")
+	cond.L.Lock()
+	go func(i int) {
+		A(i)
+		fmt.Println("finish")
+		done = true
+		cond.Signal()
+	}(1)
+	fmt.Println("start wait")
+	if !done {
+		fmt.Println("waiting")
+		cond.Wait()
+		cond.L.Unlock()
+	}
+	fmt.Println("执行完了")
+}
+
+func Fn(i int) {
+	fmt.Println("我是A", i)
+	// defer func() { //在panic后声明defer,不能捕获异常
+	// 	if err := recover(); err != nil {
+	// 		fmt.Println("恢复", err)
+	// 	} else {
+	// 		fmt.Println("没有异常")
+	// 	}
+	// 	// fmt.Println("defer")
+
+	// }()
+	panic("崩溃")
+	// fmt.Println("我是A", i)
+
+}
+
+func recoverDefer() {
+	var wg sync.WaitGroup
+	fmt.Println("我是main")
+	wg.Add(1)
+	go func(i int) {
+		defer func() { //在panic后声明defer,不能捕获异常
+			if err := recover(); err != nil {
+				fmt.Println("恢复", err)
+			} else {
+				fmt.Println("没有异常")
+			}
+			wg.Done()
+			// fmt.Println("defer")
+
+		}()
+		Fn(i)
+
+	}(1)
+	wg.Wait()
+
+	fmt.Println("执行完了")
+}
+
+func main() {
+	// SyncWaitGroupWay()
+	// SyncCondWay()
+	recoverDefer()
+}
